808dc37745da8adc646f4f6f6359ab35f46b5ec9
yisroelk <yk3281005@gmail.com>
2024-05-21 09:11:46 +0300

    ok

--- pixelate/pixelate_image.py
+++ pixelate/pixelate_image.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import numpy as np
 
 def pixelate(path, save_path):
     #open desired image
@@ -6,8 +7,8 @@ def pixelate(path, save_path):
     #find its width & height
     w,h = im.size
     #find NEW dimensions from user-defined number (50% for example)
-    new_w = 3
-    new_h = 3
+    new_w = 10
+    new_h = 10
     #downsample image to these new dimensions
     down_sampled = im.resize((new_w, new_h))
     #upsample back to original size (using "4" to signify bicubic)
@@ -15,4 +16,4 @@ def pixelate(path, save_path):
     #save the image
     up_sampled.save(save_path)
 
-pixelate("collage_poster_shows_2.jpg","new_image.jpg")
+pixelate("/Users/yisroel/האחסון שלי/Mefathim/pixelate/collage_poster_shows_2.jpg","/Users/yisroel/האחסון שלי/Mefathim/pixelate/new_image.jpg")
